2430cf29bf6780051e4a3ecaabc404bc3b6e0fa3
Damian Leijten <damian@marcleijten.nl>
2015-10-12 11:30:15 +0200

    fixed paths for all dashboard.php files

--- app/init.php
+++ app/init.php
@@ -1,15 +1,12 @@
 <?php
 
-define('SCHEME', 'http');
-
 define('ROOT', __DIR__ . '/../');
 define('PUBLIC_FOLDER', ROOT . 'public/');
 define('APP_FOLDER', ROOT . 'app/');
 define('ASSETS', PUBLIC_FOLDER . '/assets');
 
-define ('HTTP', SCHEME . '://localhost:8888/PHP-SP/');
 
-define ('HTTP_PATH', 'http://localhost:8888/Barroc_IT(site)/');
+define ('HTTP_PATH', 'http://localhost/barrocitprogram/BarrocITProgram/');
 
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/config/database.php';
--- public/views/sales/dashboard.php
+++ public/views/sales/dashboard.php
@@ -21,5 +21,5 @@
     </main>
 </div>
 
-<?php require_once __DIR__ . '/../footer.php'; ?>
+<?php require_once __DIR__ . '/../../footer.php'; ?>
 
