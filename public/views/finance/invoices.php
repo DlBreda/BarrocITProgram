94301d643077b534bcb8575d6c2ed0ac584dbd95
kimvg2 <kimvg2@hotmail.com>
2015-10-13 11:22:24 +0200

    invoices.php bijgewerkt en new-invoices.php aangemaakt
    css is opgelost. invoices nog niet af. new-invoices net aangemaakt

--- public/views/finance/invoices.php
+++ public/views/finance/invoices.php
@@ -1,4 +1,4 @@
-<?php
+<?php require_once __DIR__ . '/../../header.php';
 /**
  * Created by PhpStorm.
  * User: K
@@ -14,7 +14,9 @@
     </header>
     <?php require_once __DIR__ . '/../aside.php'; ?>
     <main>
-        <h1></h1>
+        <div class="invoices-top">
+            <h3><a href="">New invoice</a></h3>
+        </div>
         <div class="content invoices-content">
             <ul class="invoices invoices-clients">
                 <li ><a href="">klant</a></li>
