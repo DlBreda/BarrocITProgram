7631e978f9985d54d89541668b6950f93dee5092
kimvg2 <kimvg2@hotmail.com>
2015-10-13 12:41:34 +0200

    invoices.php bijgewerkt
    checkboxes staan erin met een algemene naam. wel in css bij finance !!

--- public/views/finance/invoices.php
+++ public/views/finance/invoices.php
@@ -14,17 +14,46 @@
     </header>
     <?php require_once __DIR__ . '/../aside.php'; ?>
     <main>
-        <div class="invoices-top">
-            <h3><a href="">New invoice</a></h3>
-        </div>
-        <div class="content invoices-content">
-            <ul class="invoices invoices-clients">
-                <li ><a href="">klant</a></li>
-                <li><a href="">klant</a></li>
-                <li><a href="">klant</a></li>
-                <li><a href="">klant</a></li>
-            </ul>
-        </div>
+        <main class="invoices-main">
+            <div class="invoices-top">
+                <h3><a href="new-invoice.php">New invoice</a></h3>
+            </div>
+            <div class="content invoices-content">
+                <ul class="invoices invoices-clients">
+                    <li ><a href="">klant</a></li>
+                    <li><a href="">klant</a></li>
+                    <li><a href="">klant</a></li>
+                    <li><a href="">klant</a></li>
+                </ul>
+            </div>
+        </main>
+        <main class="main-box">
+            <div class="check">
+                <h4>inactief</h4>
+                    <form action=check-box">
+                        <ul>
+                            <li><input type="checkbox" value="active"></li>
+                            <li><input type="checkbox" value="active"></li>
+                            <li><input type="checkbox" value="active"></li>
+                            <li><input type="checkbox" value="active"></li>
+                        </ul>
+                    </form>
+            </div>
+            <div class="check">
+                <h4>BKR</h4>
+                <form action=check-box">
+                    <ul>
+                        <li><input type="checkbox" value="bkr"></li>
+                        <li><input type="checkbox" value="bkr"></li>
+                        <li><input type="checkbox" value="bkr"></li>
+                        <li><input type="checkbox" value="bkr"></li>
+                    </ul>
+
+                </form>
+            </div>
+
+
+        </main>
     </main>
 </div>
 
