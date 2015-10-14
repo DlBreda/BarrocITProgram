6661baafaa9b2c4eecfc3d546616b2e170507b94
Maarten de Wolf <pilootwolfje@hotmail.com>
2015-10-14 09:51:51 +0200

    STOP Ignore...

--- app/init.php
+++ app/init.php
@@ -7,9 +7,9 @@ define('PUBLIC_FOLDER', ROOT . 'public/');
 define('APP_FOLDER', ROOT . 'app/');
 define('ASSETS', PUBLIC_FOLDER . '/assets');
 
-define ('HTTP', SCHEME . '://localhost/PHP-SP/');
+define ('HTTP', SCHEME . '://localhost:8888/PHP-SP/');
 
-define ('HTTP_PATH', 'http://localhost/barrocitprogram/BarrocITProgram/');
+define ('HTTP_PATH', 'http://localhost:8888/BarrocITProgram/');
 
 require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/config/database.php';
