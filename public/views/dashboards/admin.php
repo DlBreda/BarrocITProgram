fc74f46cfd5b2eea96ea99462809ba7c9bfa4451
kimvg2 <kimvg2@hotmail.com>
2015-10-16 10:08:02 +0200

    mappenstructuur eindelijk klaar !!

--- public/views/dashboards/admin.php
+++ public/views/dashboards/admin.php
@@ -1,4 +1,4 @@
-<?php
+<?php require_once __DIR__ . '/../../header.php';
 /**
  * Created by PhpStorm.
  * User: K
@@ -12,4 +12,20 @@ if ($_SESSION['id'] != '1')
 }
 
 ?>
+
+<div class="container">
+    <header>
+        <div class="header-welcome">
+        </div>
+    </header>
+    <?php require_once __DIR__ . '/../../aside.php'; ?>
+    <main>
+        <h1>Notificaties</h1>
+        <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat eum repellat dolore laudantium molestiae ab vitae deleniti aliquid, dolorum accusantium laboriosam debitis asperiores cum vel eos placeat facilis velit. Eaque.
+        </div>
+    </main>
+</div>
+
+<?php require_once __DIR__ . '/../../footer.php'; ?>
 
--- public/views/dashboards/finance.php
+++ public/views/dashboards/finance.php
@@ -1,7 +1,37 @@
-<?php
+<?php require_once __DIR__ . '/../../header.php';
 /**
  * Created by PhpStorm.
  * User: K
  * Date: 15-10-2015
- * Time: 10:55
+ * Time: 10:54
  */
+
+if ($_SESSION['id'] != '1')
+{
+    header('location: ');
+}
+
+?>
+
+<div class="container">
+    <header>
+        <div class="header-welcome">
+        </div>
+    </header>
+    <?php require_once __DIR__ . '/../../aside.php'; ?>
+    <main>
+        <div class="top-dashboard-finance">
+            <h1>Trailing invoices</h1>
+        </div>
+        <div class="content finance-content">
+            <ul class="invoices">
+                <li ><a href="">klant - factuur - bedrag</a></li>
+                <li><a href="">klant - factuur - bedrag</a></li>
+                <li><a href="">klant - factuur - bedrag</a></li>
+                <li><a href="">klant - factuur - bedrag</a></li>
+            </ul>
+        </div>
+    </main>
+</div>
+
+<?php require_once __DIR__ . '/../../footer.php'; ?>
