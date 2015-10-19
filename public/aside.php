6f4eeafc8ded258f0418a61f62ca8a891690efc1
Damian Leijten <damian@marcleijten.nl>
2015-10-19 11:45:09 +0200

    aside helemaal gefixed

--- public/aside.php
+++ public/aside.php
@@ -1,25 +1,89 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Maarten
- * Date: 09-10-15
- * Time: 10:00
- */
-?>
-<aside>
-    <ul>
-        <li><a href="">Dashboard</a></li>
-        <li><a href="<?= HTTP_PATH . 'public/views/customers/overview.php'?>">Customers</a></li>
-        <li><a href="<?= HTTP_PATH . 'public/views/projects/overview.php'?>">Projects</a></li>
-        <li><a href="<?= HTTP_PATH . 'public/views/invoices/overview.php'?>">Invoices</a></li>
-        <li><a href="">Appointments</a></li>
-    </ul>
+switch ($_SESSION['id']) {
+    case 1: ?>
+        <aside>
+            <ul>
+                <li><a href="">Dashboard</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/customers/overview.php' ?>">Custommers</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/projects/overview.php' ?>">Projects</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/invoices/overview.php' ?>">Invoices</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/appointments/adminAppointments.php' ?>">Appointments</a></li>
+            </ul>
 
-    <div class="bottom-aside">
-        <ul>
-            <li><a href="">Log out</a></li>
-            <li>user</li>
-        </ul>
-    </div>
-</aside>
+            <div class="bottom-aside">
+                <ul>
+                    <li><a href="">Log out</a></li>
+                    <li><?= $_SESSION['username'];?></li>
+                </ul>
+            </div>
+        </aside>
+        <?php
+        break;
+
+    case 2:?>
+        <aside>
+            <ul>
+                <li><a href="">Dashboard</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/customers/overview.php' ?>">Sales</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/projects/overview.php' ?>">Sales</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/invoices/overview.php' ?>">Invoices</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/appointments/salesAppointments.php' ?>">Appointments</a></li>
+            </ul>
+
+            <div class="bottom-aside">
+                <ul>
+                    <li><a href="">Log out</a></li>
+                    <li><?= $_SESSION['username'];?></li>
+                </ul>
+            </div>
+        </aside>
+        <?php
+        break;
+
+    case 3:?>
+        <aside>
+            <ul>
+                <li><a href="">Dashboard</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/customers/overview.php' ?>">Sales</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/projects/overview.php' ?>">Sales</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/invoices/overview.php' ?>">Invoices</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/appointments/financeAppointments.php' ?>">Appointments</a></li>
+            </ul>
+
+            <div class="bottom-aside">
+                <ul>
+                    <li><a href="">Log out</a></li>
+                    <li><?= $_SESSION['username'];?></li>
+                </ul>
+            </div>
+        </aside>
+        <?php
+        break;
+
+    case 4:?>
+        <aside>
+            <ul>
+                <li><a href="">Dashboard</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/customers/overview.php' ?>">Sales</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/projects/overview.php' ?>">Sales</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/invoices/overview.php' ?>">Invoices</a></li>
+                <li><a href="<?= HTTP_PATH . 'public/views/appointments/developmentAppointments.php' ?>">Appointments</a></li>
+            </ul>
+
+            <div class="bottom-aside">
+                <ul>
+                    <li><a href="">Log out</a></li>
+                    <li><?= $_SESSION['username'];?></li>
+                </ul>
+            </div>
+        </aside>
+        <?php
+        break;
+
+    default:
+        header('location:' . HTTP_PATH . 'public/');
+
+}
+?>
+
 
---
+++ public/views/appointments/adminAppointments.php
@@ -0,0 +1,13 @@
+<?php require_once __DIR__ . '/../../header.php';
+/**
+ * Created by PhpStorm.
+ * User: Leijten
+ * Date: 19-10-2015
+ * Time: 11:36
+ */
+
+echo $_SESSION['username'];
+echo "<br>";
+echo "test";
+
+require_once __DIR__ . '/../../footer.php'; ?>
