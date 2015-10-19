19ca27167ba2cc4fe116ba0b3c8926fe5f54394f
kimvg2 <kimvg2@hotmail.com>
2015-10-19 11:09:56 +0200

    customers/show.php require erin gezet

--- public/views/customers/show.php
+++ public/views/customers/show.php
@@ -1,8 +1,19 @@
-<?php
+<?php require_once __DIR__ . '/../../header.php';
 /**
  * Created by PhpStorm.
  * User: K
  * Date: 15-10-2015
  * Time: 10:12
  */
+?>
+
+
+
+
+
+
+
+<?php require_once __DIR__ . '/../../footer.php'; ?>
+
+
 
